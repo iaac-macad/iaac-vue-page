0582729cfd7a6f24eeec5308d44a05bc48a95943
just-ajs <35227625+just-ajs@users.noreply.github.com>
2022-12-20 11:04:18 +0000

    remove hello world

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>iaac testing vue</h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
